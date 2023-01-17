1c232cfdc784435adfa914b19119c1bb364e2ee2
minwoo.kim <minu1540@gmail.com>
2023-01-17 21:16:15 +0900

    chore : 작성 코드 추가

--- memo/6.11.js
+++ memo/6.11.js
@@ -23,4 +23,124 @@ function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
   const shippingPerCase = (basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+
+  return price
+}
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, discount);
+
+  return price;
+}
+
+// 두 번째 단계를 처리하는 함수
+function applyShipping(basePrice, shippingMethod, discount) {
+  const shippingPerCase = (basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+
+  return price;
+}
+
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
+  const priceData = {}; // 중간 데이터 구조
+  const price = applyShipping(priceData, basePrice, shippingMethod, discount);
+
+  return price;
+}
+
+function applyShipping(priceData, basePrice, shippingMethod, discount) {
+  const shippingPerCase = (basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+
+  return price;
+}
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
+  const priceData = {basePrice}; 
+  const price = applyShipping(priceData, shippingMethod, quantity, discount);
+
+  return price;
+}
+
+function applyShipping(priceData, shippingMethod, quantity, discount) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = quantity * shippingPerCase;
+  const price = priceData.basePrice - discount + shippingCost;
+
+  return price;
+}
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
+  const priceData = {basePrice, quantity, discount}; 
+  const price = applyShipping(priceData, shippingMethod);
+
+  return price;
+}
+
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
+
+  return price;
+}
+
+
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+
+  return price;
+}
+
+// 첫 번째 단계를 처리 하는 함수
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
+
+  return {basePrice, quantity, discount}; 
+}
+
+// 두 번째 단계를 처리 하는 함수
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
+
+  return price;
+}
+
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+
+  return applyShipping(priceData, shippingMethod);
+}
+
+// 첫 번째 단계를 처리 하는 함수
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold) * product.basePrice * product.discountRate;
+
+  return {basePrice, quantity, discount}; 
+}
+
+// 두 번째 단계를 처리 하는 함수
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee :  shippingMethod.feePerCase);
+  const shippingCost = priceData.quantity * shippingPerCase;
+
+  return priceData.basePrice - priceData.discount + shippingCost;
 }
