4017d7010469c32596c5b8efaa8ce9a646e6e388
minwoo.kim <minu1540@gmail.com>
2023-01-29 23:27:42 +0900

    chore : 예시 추가

--- memo/7.1.js
+++ memo/7.1.js
@@ -89,3 +89,103 @@ class Organization {
     return this._country;
   }
 }
+
+'1920' : {
+  name : '마틴 파울러',
+  id : '1920',
+  usages : {
+    '2016' : {
+      '1': 50,
+      '2' : 50
+    }
+  }
+}
+
+// 쓰기
+customerData[customerId].usages[year][month] = amount;
+
+// 읽기
+function compareUsage(customerId, laterYear, month) {
+  const later = customerData[customerId].usages[laterYear][month];
+  const earlier = customerData[customerId].usages[laterYear - 1][month];
+
+  return {laterAmount : later, change : later - earlier};
+}
+
+function getRawDataOfCustomers() {
+  return customerData;
+}
+
+function setRawDataOfCustomers(arg) {
+  customerData = arg;
+}
+
+// 쓰기
+getRawDataOfCustomers()[customerId].usages[year][month] = amount;
+
+// 읽기
+function compareUsage(customerId, laterYear, month) {
+  const later = getRawDataOfCustomers()[customerId].usages[laterYear][month];
+  const earlier = getRawDataOfCustomers()[customerId].usages[laterYear - 1][month];
+
+  return {laterAmount : later, change : later - earlier};
+}
+
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+function getCustomerData() {
+  return customerData;
+}
+
+function getRawDataOfCustomers() {
+  return customerData._data;;
+}
+
+function setRawDataOfCustomers(arg) {
+  customerData = new CustomerData(arg);
+}
+
+function setUsage(customerId, year, month, amount) {
+  getRawDataOfCustomers()[customerId].usages[year][month] = amount;
+}
+
+setUsage(customerId, year, month, amount);
+
+
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+
+  setUsage(customerId, year, month, amount) {
+    this._data[customerId].usages[year][month] = amount;
+  }
+}
+
+getCustomerData().setUsage(customerId, year, month, amount);
+
+
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+
+  usage(customerId, year, month) {
+    return this._data[customerId].usages[year][month];
+  }
+}
+
+
+
+
+// 읽기
+function compareUsage(customerId, laterYear, month) {
+  const later = getCustomerData().usage(customerId, laterYear, month);
+  const earlier = getCustomerData().usage(customerId, laterYear - 1, month)
+
+  return {laterAmount : later, change : later - earlier};
+}
